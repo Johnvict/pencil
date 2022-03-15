164d7bebbb9b1d8ca7656134a883a0979053ac80
Johnvict <johnvict20@gmail.com>
2022-03-15 17:37:17 +0100

    feat: implement sort question numbers

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -10,56 +10,66 @@ const search = async (req, res) => {
   try {
     const aggregationResult = await Topic.aggregate([
       {
-        $match: {
-          value: { $regex: req.query.q },
-        },
-      },
-      {
-        $graphLookup: {
-          from: 'topics',
-          startWith: '$_id',
-          connectFromField: '_id',
-          connectToField: 'parent_topic',
-          as: 'sub_topics',
-          maxDepth: 1,
-        },
-      },
-      {
-        $unwind: '$sub_topics',
-      },
-
-      {
-        $project: {
-          _id: 1,
-          main_topic: '$value',
-          sub_topic_id: '$sub_topics._id',
-          parent_id: '$_id',
-        },
-      },
-
-      {
+        '$match': {
+            'value': 'Describe and carry out tests for'
+            //            value: { $regex: "Describe and carry out tests for" }
+        }
+    },
+    {
+        '$graphLookup': {
+            'from': 'topics',
+            'startWith': '$_id',
+            'connectFromField': '_id',
+            'connectToField': 'parent_topic',
+            'as': 'sub_topics',
+            'maxDepth': 1
+        }
+    },
+    {
+        $unwind: "$sub_topics",
+    },
+    {
         $lookup: {
-          from: 'questions',
-          localField: 'sub_topic_id',
-          foreignField: 'annotations',
-          as: 'questions',
-        },
-      },
-      {
-        $unwind: '$questions',
-      },
-      {
-        $group: {
-          _id: '$_id',
-          questions: { $addToSet: '$questions.question_number' },
-        },
-      },
-      {
+            from: "questions",
+            localField: "sub_topic_id",
+            foreignField: "annotations",
+            as: "questions",
+        }
+    },
+
+    {
         $project: {
-          _id: 1,
-          questions: '$questions',
-        },
-      },
+            _id: 1,
+            main_topic: "$value",
+            sub_topic_id: "$sub_topics._id",
+            parent_id: "$_id"
+        }
+    },
+
+    {
+        $lookup: {
+            from: "questions",
+            localField: "sub_topic_id",
+            foreignField: "annotations",
+            as: "questions",
+        }
+    },
+    {
+        $unwind: "$questions",
+    },
+    {
+        $group: {
+            _id: "$_id",
+            questions: { $addToSet: "$questions.question_number" }
+        }
+    },
+    {
+        $unwind: "$questions"
+    },
+    {
+        $sort: { _id: 1, questions: 1 }
+    },
+    { "$group": { "_id": "$_id", "questions": { "$push": "$questions" } } }
     ]);
 
     return res.status(200).json({
